cda0fdfaaf6b0a0f06da3a22126235027b0f6b64
ortizjd-jmu <ortizjd@dukes.jmu.edu>
2020-01-16 16:01:44 -0500

    Took token out

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const TOKEN = "NjY1Njk5MDUxOTQ5MzkxODkz.XhpbBA.29lgqMTrR8mmr2lklCPSfqhNGVA";
+const TOKEN = "Place Token here";
 
 // Bot responds to "!code" and sends the correct code
-bot.on("message", function(message) {
+bot.on("message", function (message) {
   if (message.channel.type === "dm") {
     message.author.send("Nice Try. Please use the proper channel.");
   } else if (message.content == "!code") {
     var fs = require("fs");
-    
+
     // Bot pulls code from InviteCode.txt file and sends the data to the user
     fs.readFile(
       "C://Program Files (x86)/Steam/steamapps/common/Stardew Valley/Mods/Always On Server/InviteCode.txt",
       "utf8",
-      function(err, data) {
+      function (err, data) {
         if (err) throw err;
         console.log(data);
         message.author.send(data);
